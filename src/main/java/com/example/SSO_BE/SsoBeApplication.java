74c6613ea6b6b1ff13e35ec32c0d5be8a2c15f58
MSI\USER <tonkla785@gmail.com>
2025-12-09 15:09:12 +0700

    create-project

---
+++ src/main/java/com/example/SSO_BE/SsoBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.SSO_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsoBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsoBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SSO_BE/SsoBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SSO_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsoBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
